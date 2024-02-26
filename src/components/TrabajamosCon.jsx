365c44e8728abe535c87ed3e992834a7812557ff
John Mata <john.mata@epn.edu.ec>
2024-02-25 20:18:11 -0500

    Primeras Páginas

---
+++ src/components/TrabajamosCon.jsx
@@ -0,0 +1,19 @@
+function TrabajamosCon() {
+    return (
+        <>
+            <ul class="flex justify-center items-center">
+                <li><img src="/assets/images/icons/samsung.png" alt="Samsung" /></li>
+                <li><img src="/assets/images/icons/lg.png" alt="LG" /></li>
+                <li><img src="/assets/images/icons/sony.png" alt="Sony" /></li>
+                <li><img src="/assets/images/icons/apple.png" alt="Apple" /></li>
+                {/* <li><img src="" alt="Dell" /></li>
+                <li><img src="" alt="HP" /></li>
+                <li><img src="" alt="Lenovo" /></li>
+                <li><img src="" alt="Acer" /></li>
+                <li><img src="" alt="Asus" /></li> */}
+            </ul>
+        </>
+    );
+}
+
+export default TrabajamosCon;
