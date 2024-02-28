2db38911321b6ebfb050657c76138b7531957a4f
John Mata <john.mata@epn.edu.ec>
2024-02-28 01:46:34 -0500

    Productos Relacionados

--- src/data/dataHardware.js
+++ src/data/dataHardware.js
@@ -4,83 +4,107 @@ export default [
 		nombre: 'Dark Rock 4 Air Cooler',
 		precio: 74.99,
 		descripcion: 'El Dark Rock 4 es un disipador de CPU de una sola torre con un ventilador de 135 mm. Ofrece un rendimiento de refrigeración extremadamente alto de 200 W TDP y un funcionamiento prácticamente inaudible. El diseño compacto de una sola torre ahorra espacio y se adapta a prácticamente todos los sistemas de PC. Con un diseño de aletas asimétricas y seis tubos de calor, el Dark Rock 4 proporciona un enfriamiento silencioso y eficiente.',
-		src: '/assets/images/products/HAR0291.webp'
+		src: '/assets/images/products/HAR0291.webp',
+		categoria: 'hardware',
+		subcategoria: 'coolers'
 	},
 	{
 		id: 'HAR0459',
 		nombre: 'Memoria RAM Corsair Vengeance LPX 16GB DDR4',
 		precio: 89.99,
-		descipcion: 'La memoria DDR4 Vengeance LPX se ha diseñado para overclocking de alto rendimiento. El disipador de calor, fabricado en aluminio puro, permite una disipación térmica más rápida; la placa impresa de ocho capas administra el calor y proporciona una capacidad superior para incrementar el overclocking. Cada circuito integrado se selecciona individualmente para el máximo potencial de rendimiento.',
-		src: '/assets/images/products/HAR0459.webp'
+		descripcion: 'La memoria DDR4 Vengeance LPX se ha diseñado para overclocking de alto rendimiento. El disipador de calor, fabricado en aluminio puro, permite una disipación térmica más rápida; la placa impresa de ocho capas administra el calor y proporciona una capacidad superior para incrementar el overclocking. Cada circuito integrado se selecciona individualmente para el máximo potencial de rendimiento.',
+		src: '/assets/images/products/HAR0459.webp',
+		categoria: 'hardware',
+		subcategoria: 'memorias'
 	},
 	{
 		id: 'HAR0673',
 		nombre: 'Tarjeta Gráfica MSI GeForce RTX 3060 Ti',
 		precio: 399.99,
 		descripcion: 'La GeForce RTX 3060 Ti es una tarjeta gráfica de alto rendimiento que ofrece una potencia increíble para los juegos en 1080p y 1440p. Basada en la arquitectura NVIDIA Ampere, la RTX 3060 Ti cuenta con 4864 núcleos CUDA y 8 GB de memoria GDDR6. La tarjeta gráfica MSI GeForce RTX 3060 Ti Ventus 2X OC cuenta con un diseño de doble ventilador para una refrigeración eficiente y un rendimiento silencioso.',
-		src: '/assets/images/products/HAR0673.webp'
+		src: '/assets/images/products/HAR0673.webp',
+		categoria: 'hardware',
+		subcategoria: 'tarjetas_graficas'
 	},
 	{
 		id: 'HAR0789',
 		nombre: 'Fuente de Alimentación Corsair RM750x 750W 80 Plus',
 		precio: 119.99,
 		descripcion: 'La fuente de alimentación Corsair RM750x es una fuente de alimentación modular con certificación 80 Plus Gold. Ofrece una eficiencia energética del 90% y un funcionamiento silencioso. El ventilador de 135 mm cuenta con un modo de ventilador sin parada para un funcionamiento silencioso y una refrigeración eficiente. La fuente de alimentación RMx Series cuenta con condensadores japoneses de alta calidad para un rendimiento fiable y una larga vida útil.',
-		src: '/assets/images/products/HAR0789.webp'
+		src: '/assets/images/products/HAR0789.webp',
+		categoria: 'hardware',
+		subcategoria: 'fuentes_alimentacion'
 	},
 	{
 		id: 'HAR0895',
 		nombre: 'Placa Base ASUS ROG Strix B550-F Gaming',
 		precio: 179.99,
 		descripcion: 'La placa base ASUS ROG Strix B550-F Gaming es una placa base ATX con un diseño de alimentación digital de 12+2 fases y un disipador de calor VRM para un rendimiento fiable y una refrigeración eficiente. La placa base cuenta con dos ranuras M.2, USB 3.2 Gen 2, Wi-Fi 6 y 2.5 Gb Ethernet. El diseño de audio SupremeFX S1220A proporciona una calidad de sonido excepcional para una experiencia de juego inmersiva.',
-		src: '/assets/images/products/HAR0895.webp'
+		src: '/assets/images/products/HAR0895.webp',
+		categoria: 'hardware',
+		subcategoria: 'placas_base'
 	},
 	{
 		id: 'HAR1001',
 		nombre: 'Disco Duro SSD Samsung 970 EVO Plus 1TB',
 		precio: 199.99,
 		descripcion: 'El disco duro SSD Samsung 970 EVO Plus es un disco de estado sólido NVMe M.2 con una capacidad de 1 TB. Ofrece velocidades de lectura y escritura secuenciales de hasta 3500 MB/s y 3300 MB/s, respectivamente. El disco SSD 970 EVO Plus cuenta con la tecnología V-NAND de Samsung y el controlador Phoenix para un rendimiento excepcional y una fiabilidad duradera.',
-		src: '/assets/images/products/HAR1001.webp'
+		src: '/assets/images/products/HAR1001.webp',
+		categoria: 'hardware',
+		subcategoria: 'discos_duros'
 	},
 	{
 		id: 'HAR1111',
 		nombre: 'Caja de PC NZXT H510',
 		precio: 69.99,
 		descripcion: 'La caja de PC NZXT H510 es una caja compacta ATX con un diseño elegante y minimalista. La caja cuenta con un panel frontal de E/S USB 3.1 Gen 2 Tipo-C, un panel lateral de vidrio templado y un sistema de gestión de cables integrado. La caja H510 cuenta con dos ventiladores Aer F de 120 mm para una refrigeración eficiente y un rendimiento silencioso.',
-		src: '/assets/images/products/HAR1111.webp'
+		src: '/assets/images/products/HAR1111.webp',
+		categoria: 'hardware',
+		subcategoria: 'cajas_pc'
 	},
 	{
 		id: 'HAR1221',
 		nombre: 'Procesador AMD Ryzen 5 5600X',
 		precio: 299.99,
 		descripcion: 'El procesador AMD Ryzen 5 5600X es un procesador de 6 núcleos y 12 hilos con una velocidad de reloj base de 3.7 GHz y una velocidad de reloj máxima de 4.6 GHz. El procesador cuenta con la arquitectura Zen 3 de AMD para un rendimiento excepcional en aplicaciones de productividad y juegos. El procesador Ryzen 5 5600X es compatible con el socket AM4 y cuenta con una caché L3 de 32 MB.',
-		src: '/assets/images/products/HAR1221.webp'
+		src: '/assets/images/products/HAR1221.webp',
+		categoria: 'hardware',
+		subcategoria: 'procesadores'
 	},
 	{
 		id: 'HAR1331',
 		nombre: 'Ventilador de Caja Corsair LL120 RGB',
 		precio: 39.99,
 		descripcion: 'El ventilador de caja Corsair LL120 RGB es un ventilador de 120 mm con iluminación RGB. El ventilador cuenta con 16 LED RGB independientes en cada aspa para una iluminación vibrante y personalizable. El ventilador LL120 RGB ofrece un flujo de aire de hasta 43.25 CFM y una presión estática de hasta 1.61 mmH2O para una refrigeración eficiente y un rendimiento silencioso.',
-		src: '/assets/images/products/HAR1331.webp'
+		src: '/assets/images/products/HAR1331.webp',
+		categoria: 'hardware',
+		subcategoria: 'ventiladores'
 	},
 	{
 		id: 'HAR1441',
 		nombre: 'Tarjeta de Sonido Creative Sound Blaster Z',
 		precio: 99.99,
 		descripcion: 'La tarjeta de sonido Creative Sound Blaster Z es una tarjeta de sonido PCIe con un procesador de audio SBX Pro Studio para una calidad de sonido excepcional. La tarjeta de sonido cuenta con un amplificador de auriculares de 600 ohmios para una potencia de salida superior y una relación señal/ruido de 116 dB para un sonido nítido y claro. La tarjeta de sonido Sound Blaster Z es compatible con el software de control de audio Sound Blaster Command para una personalización avanzada.',
-		src: '/assets/images/products/HAR1441.png'
+		src: '/assets/images/products/HAR1441.png',
+		categoria: 'hardware',
+		subcategoria: 'tarjetas_sonido'
 	},
 	{
 		id: 'HAR1551',
 		nombre: 'Refrigeración Líquida Corsair H100i RGB Platinum SE',
 		precio: 149.99,
 		descripcion: 'La refrigeración líquida Corsair H100i RGB Platinum SE es un sistema de refrigeración líquida de doble ventilador con iluminación RGB. El radiador de 240 mm cuenta con dos ventiladores ML120 RGB de 120 mm para una refrigeración eficiente y un rendimiento silencioso. El cabezal de bombeo cuenta con iluminación RGB dinámica para una estética impresionante. El sistema de refrigeración líquida H100i RGB Platinum SE es compatible con el software de control de refrigeración líquida Corsair iCUE para una personalización avanzada.',
-		src: '/assets/images/products/HAR1551.png'
+		src: '/assets/images/products/HAR1551.png',
+		categoria: 'hardware',
+		subcategoria: 'refrigeracion_liquida'
 	},
 	{
 		id: 'HAR1661',
 		nombre: 'Tarjeta de Captura Elgato Game Capture HD60 S+',
 		precio: 199.99,
 		descripcion: 'La tarjeta de captura Elgato Game Capture HD60 S+ es una tarjeta de captura externa con una resolución de hasta 1080p60 HDR y una tasa de bits de hasta 60 Mbps. La tarjeta de captura cuenta con una entrada HDMI y una salida HDMI para una conexión sencilla a tu consola de juegos y tu monitor. La tarjeta de captura HD60 S+ es compatible con el software de grabación y transmisión de Elgato para una experiencia de captura de juegos sin problemas.',
-		src: '/assets/images/products/HAR1661.webp'
+		src: '/assets/images/products/HAR1661.webp',
+		categoria: 'hardware',
+		subcategoria: 'tarjetas_captura'
 	},
 ]
--- src/data/dataLaptops.js
+++ src/data/dataLaptops.js
@@ -4,83 +4,107 @@ export default [
         nombre: 'Portátil MSI GF65 Thin 10UE-047XES',
         precio: 999.99,
         descripcion: 'El portátil MSI GF65 Thin 10UE-047XES es un portátil gaming de alto rendimiento con un diseño elegante y ligero. Cuenta con un procesador Intel Core i7 de 10ª generación, 16 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce RTX 3060. El portátil cuenta con una pantalla Full HD de 15.6 pulgadas, un disco duro SSD de 512 GB y un teclado SteelSeries con retroiluminación RGB.',
-        src: '/assets/images/products/LAP0114.jpg'
+        src: '/assets/images/products/LAP0114.jpg',
+        categoria: 'laptops',
+		subcategoria: 'msi'
 	},
 	{
 		id: 'LAP0228',
         nombre: 'Portátil HP Pavilion 15-eg0002ns',
         precio: 799.99,
         descripcion: 'El portátil HP Pavilion 15-eg0002ns es un portátil ligero y versátil con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador Intel Core i5 de 11ª generación, 8 GB de memoria RAM DDR4 y un disco duro SSD de 512 GB. El portátil HP Pavilion 15-eg0002ns es ideal para el trabajo, el entretenimiento y las tareas diarias.',
-        src: '/assets/images/products/LAP0228.jpg'
+        src: '/assets/images/products/LAP0228.jpg',
+        categoria: 'laptops',
+        subcategoria: 'hp'
 	},
     {
         id: 'LAP0332',
         nombre: 'Portátil Lenovo Legion 5 15ACH6H',
         precio: 899.99,
         descripcion: 'El portátil Lenovo Legion 5 15ACH6H es un portátil gaming de alto rendimiento con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador AMD Ryzen 7 5800H, 16 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce RTX 3060. El portátil cuenta con un disco duro SSD de 512 GB, un teclado retroiluminado y un sistema de refrigeración avanzado.',
-        src: '/assets/images/products/LAP0332.jpg'
+        src: '/assets/images/products/LAP0332.jpg',
+        categoria: 'laptops',
+        subcategoria: 'lenovo'
     },
     {
         id: 'LAP0446',
         nombre: 'Portátil ASUS TUF Gaming A15 FA506QM-HN008',
         precio: 1199.99,
         descripcion: 'El portátil ASUS TUF Gaming A15 FA506QM-HN008 es un portátil gaming de alto rendimiento con un diseño resistente y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador AMD Ryzen 7 5800H, 16 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce RTX 3060. El portátil cuenta con un disco duro SSD de 1 TB, un teclado retroiluminado y una batería de larga duración.',
-        src: '/assets/images/products/LAP0446.jpg'
+        src: '/assets/images/products/LAP0446.jpg',
+        categoria: 'laptops',
+        subcategoria: 'asus'
     },
     {
         id: 'LAP0550',
         nombre: 'Portátil Acer Nitro 5 AN515-45-R8X2',
         precio: 999.99,
         descripcion: 'El portátil Acer Nitro 5 AN515-45-R8X2 es un portátil gaming de alto rendimiento con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador AMD Ryzen 7 5800H, 16 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce RTX 3060. El portátil cuenta con un disco duro SSD de 1 TB, un teclado retroiluminado y un sistema de refrigeración avanzado.',
-        src: '/assets/images/products/LAP0550.webp'
+        src: '/assets/images/products/LAP0550.webp',
+        categoria: 'laptops',
+        subcategoria: 'acer'
     },
     {
         id: 'LAP0664',
         nombre: 'Portátil MSI GF63 Thin 10SCSR-876XES',
         precio: 899.99,
         descripcion: 'El portátil MSI GF63 Thin 10SCSR-876XES es un portátil ligero y versátil con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador Intel Core i7 de 10ª generación, 16 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce GTX 1650 Ti. El portátil cuenta con un disco duro SSD de 512 GB, un teclado retroiluminado y una batería de larga duración.',
-        src: '/assets/images/products/LAP0664.jpg'
+        src: '/assets/images/products/LAP0664.jpg',
+        categoria: 'laptops',
+        subcategoria: 'msi'
     },
     {
         id: 'LAP0778',
         nombre: 'Portátil HP Pavilion Gaming 15-ec2004ns',
         precio: 799.99,
         descripcion: 'El portátil HP Pavilion Gaming 15-ec2004ns es un portátil gaming de alto rendimiento con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador AMD Ryzen 5 4600H, 8 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce GTX 1650. El portátil cuenta con un disco duro SSD de 512 GB, un teclado retroiluminado y un sistema de refrigeración avanzado.',
-        src: '/assets/images/products/LAP0778.jpg'
+        src: '/assets/images/products/LAP0778.jpg',
+        categoria: 'laptops',
+        subcategoria: 'hp'
     },
     {
         id: 'LAP0882',
         nombre: 'Portátil ASUS ROG Strix G15 G513QM-HN218',
         precio: 1299.99,
         descripcion: 'El portátil ASUS ROG Strix G15 G513QM-HN218 es un portátil gaming de alto rendimiento con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador AMD Ryzen 7 5800H, 16 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce RTX 3060. El portátil cuenta con un disco duro SSD de 1 TB, un teclado retroiluminado y un sistema de refrigeración avanzado.',
-        src: '/assets/images/products/LAP0882.png'
+        src: '/assets/images/products/LAP0882.png',
+        categoria: 'laptops',
+        subcategoria: 'asus'
     },
     {
         id: 'LAP0996',
         nombre: 'Portátil Lenovo IdeaPad Gaming 3 15ARH05',
         precio: 699.99,
         descripcion: 'El portátil Lenovo IdeaPad Gaming 3 15ARH05 es un portátil gaming de alto rendimiento con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador AMD Ryzen 5 4600H, 8 GB de memoria RAM DDR4 y una tarjeta gráfica NVIDIA GeForce GTX 1650. El portátil cuenta con un disco duro SSD de 512 GB, un teclado retroiluminado y un sistema de refrigeración avanzado.',
-        src: '/assets/images/products/LAP0996.jpg'
+        src: '/assets/images/products/LAP0996.jpg',
+        categoria: 'laptops',
+        subcategoria: 'lenovo'
     },
     {
         id: 'LAP1000',
         nombre: 'Portátil Acer Aspire 5 A515-56-50RS',
         precio: 599.99,
         descripcion: 'El portátil Acer Aspire 5 A515-56-50RS es un portátil ligero y versátil con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador Intel Core i5 de 11ª generación, 8 GB de memoria RAM DDR4 y un disco duro SSD de 512 GB. El portátil Acer Aspire 5 A515-56-50RS es ideal para el trabajo, el entretenimiento y las tareas diarias.',
-        src: '/assets/images/products/LAP1000.webp'
+        src: '/assets/images/products/LAP1000.webp',
+        categoria: 'laptops',
+        subcategoria: 'acer'
     },
     {
         id: 'LAP1110',
         nombre: 'Portátil ASUS VivoBook 15 F515EA-BQ253T',
         precio: 499.99,
         descripcion: 'El portátil ASUS VivoBook 15 F515EA-BQ253T es un portátil ligero y versátil con un diseño elegante y una pantalla Full HD de 15.6 pulgadas. Cuenta con un procesador Intel Core i3 de 11ª generación, 8 GB de memoria RAM DDR4 y un disco duro SSD de 256 GB. El portátil ASUS VivoBook 15 F515EA-BQ253T es ideal para el trabajo, el entretenimiento y las tareas diarias.',
-        src: '/assets/images/products/LAP1110.jpg'
+        src: '/assets/images/products/LAP1110.jpg',
+        categoria: 'laptops',
+        subcategoria: 'asus'
     },
     {
         id: 'LAP1224',
         nombre: 'Portátil HP 15s-fq2032ns',
         precio: 399.99,
         descripcion: 'El portátil HP 15s-fq2032ns es un portátil ligero y versátil con un diseño elegante y una pantalla HD de 15.6 pulgadas. Cuenta con un procesador Intel Core i3 de 11ª generación, 8 GB de memoria RAM DDR4 y un disco duro SSD de 256 GB. El portátil HP 15s-fq2032ns es ideal para el trabajo, el entretenimiento y las tareas diarias.',
-        src: '/assets/images/products/LAP1224.jpg'
+        src: '/assets/images/products/LAP1224.jpg',
+        categoria: 'laptops',
+        subcategoria: 'hp'
     }
 ]
