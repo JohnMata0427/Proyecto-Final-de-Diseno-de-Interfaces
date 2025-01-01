94b90e2367b67b65e23f56b891db0943ddd69a32
JohnMata0427 <jhonmata0427@gmail.com>
2024-12-31 21:28:00 -0500

    Upgrade to Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,13 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
-
-import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()],
-    output: 'server',
-    adapter: vercel({
-        imageService: true,
-    }),
-    vite: {
-        resolve: {
-            alias: {
-                '@': path.resolve('./src'),
-            },
-        },
-        optimizeDeps: {
-            include: ['react-compiler-runtime'],
-        },
-    },
+  integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel({ imageService: true }),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,42 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {
-            colors: {
-                'morado-primario': {
-                    DEFAULT: '#2e234d',
-                    100: '#7d6b9e',
-                    200: '#6f5e8e',
-                    300: '#62517e',
-                    400: '#54446e',
-                    500: '#47375e',
-                    600: '#3a2a4e',
-                    700: '#2c1d3e',
-                    800: '#1f103e',
-                    900: '#11032e',
-                },
-                'gris-oscuro': {
-                    DEFAULT: '#202020',
-                    100: '#5a5a5a',
-                    200: '#4d4d4d',
-                    300: '#404040',
-                    400: '#333333',
-                    500: '#262626',
-                    600: '#191919',
-                    700: '#0c0c0c',
-                },
-            },
-            animation: {
-                scroll: 'scroll 20s linear infinite',
-            },
-            keyframes: {
-                scroll: {
-                    '0%': { transform: 'translateX(0%)' },
-                    '100%': { transform: 'translateX(-100%)' },
-                },
-            },
-        },
-    },
-    plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+				colors: {
+						'morado-primario': {
+								DEFAULT: '#2e234d',
+								100: '#7d6b9e',
+								200: '#6f5e8e',
+								300: '#62517e',
+								400: '#54446e',
+								500: '#47375e',
+								600: '#3a2a4e',
+								700: '#2c1d3e',
+								800: '#1f103e',
+								900: '#11032e',
+						},
+						'gris-oscuro': {
+								DEFAULT: '#202020',
+								100: '#5a5a5a',
+								200: '#4d4d4d',
+								300: '#404040',
+								400: '#333333',
+								500: '#262626',
+								600: '#191919',
+								700: '#0c0c0c',
+						},
+				},
+				animation: {
+						scroll: 'scroll 20s linear infinite',
+				},
+				keyframes: {
+						scroll: {
+								'0%': { transform: 'translateX(0%)' },
+								'100%': { transform: 'translateX(-100%)' },
+						},
+				},
+		},
+},
+	plugins: [],
+}
